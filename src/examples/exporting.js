21d810e2e1185d159921c7ded0c1624ba2e836b6
Justin Whitney <43628307+JMWhitney@users.noreply.github.com>
2018-12-11 23:21:14 -0800

    Add files via upload
    Initial commit

---
+++ src/examples/exporting.js
@@ -0,0 +1,56 @@
+//
+export default class MyComponent {
+
+}
+
+
+
+
+
+
+
+
+
+//
+// export default function() {
+//   //get the data
+// }
+//
+//
+//
+// export default const mySetup = {      //DOESNT WORK
+//   environment: 'tropical'
+// }
+//
+//
+//
+//
+// const mySetup = {
+//   environment: 'tropical'
+// }
+//
+//
+// export default mySetup
+//
+//
+//
+//
+//
+// export const widget = { /* ... */}
+//
+// export function birthdayReminder () {
+//   /* ... */
+//   return 'Happy Birthday!'
+// }
+//
+// let trees = []
+//
+// class ShinyButton extends Component {
+//  render() {
+//    return (
+//      //...
+//    )
+//  }
+// }
+//
+// export {trees, ShinyButton}
---
+++ src/examples/importing.js
@@ -0,0 +1,59 @@
+//
+import SomeRandomThing from './exporting.js'
+
+
+
+
+
+
+
+// import SomeRandomThing from './exporting.js'
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+//
+//
+// import defaultThing from './exporting'
+//
+//
+//
+//
+// import defaultThing as somethingElse from './exporting'
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// import defaultThing, {widget as anotherThing, birthdayReminder, shinyButton, trees} from './exporting'
+//
+//
+// export function importing() {
+//
+// }
